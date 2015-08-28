aeab6f5c16df086a084014e69019ca983f468e5d
Diliara Nasirova <contact@diliaranasirova.com>
2015-08-28 15:21:47 -0700

    preset semesters info + refactor

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -5,44 +5,100 @@ Date.prototype.dayOfYear = function () {
 }
 
 angular.module('datesCalcApp')
-    .controller('datesCalcController', function ($scope) {
+    .controller('datesCalcController', ['$scope', function ($scope) {
 
-        $scope.conversion = {
-            dStartSemesterFirstDay: new Date(),
-            dDestinationSemesterFirstDay: new Date(),
-            destinationDate: new Date(),
-            outcomeDate: "",
-            dNumberOfDaysDifference: ""
+
+        $scope.semesters = {
+
+            fromSemesters: [
+
+                {
+                    name: "2013 Fall - Sept 09",
+                    startDate: new Date('2013-09-09')
+                },
+                {
+                    name: "2013 Fall Int 2 - Oct 28",
+                    startDate: new Date('2013-10-28')
+                },
+                {
+                    name: "2014 Spring - Jan 13",
+                    startDate: new Date('2014-01-13')
+                },
+                {
+                    name: "2014 Summer - May 05",
+                    startDate: new Date('2014-05-05')
+                },
+                {
+                    name: "2014 Fall - Sept 02",
+                    startDate: new Date('2014-09-02')
+                },
+                {
+                    name: "2015 Spring - Jan 06",
+                    startDate: new Date('2015-01-06')
+                },
+                {
+                    name: "2015 Summer - May 04",
+                    startDate: new Date('2015-05-04')
+                    /* startDateFormatted: new Date('2015-05-04').toUTCString().slice(0, 17) */
+                }
+            ],
+            toSemesters: [
+                {
+                    name: "2013 Fall - Sept 09",
+                    startDate: new Date('2013-09-09')
+                },
+                {
+                    name: "2013 Fall Int 2 - Oct 28",
+                    startDate: new Date('2013-10-28')
+                },
+                {
+                    name: "2014 Spring - Jan 13",
+                    startDate: new Date('2014-01-13')
+                },
+                {
+                    name: "2014 Summer - May 05",
+                    startDate: new Date('2014-05-05')
+                },
+                {
+                    name: "2014 Fall - Sept 02",
+                    startDate: new Date('2014-09-02')
+                },
+                {
+                    name: "2015 Spring - Jan 06",
+                    startDate: new Date('2015-01-06')
+                },
+                {
+                    name: "2015 Summer - May 04",
+                    startDate: new Date('2015-05-04')
+                }
+            ]
+
         }
 
 
-        $scope.getDateInDestinationSemester = function (dateInStartSemester) {
-            $scope.conversion.destinationDate = new Date();
+        $scope.getDateInDestinationSemester = function (fromSemesterStartDate, toSemesterStartDate, dateInStartSemester) {
+            $scope.destinationDate = new Date();
 
-            $scope.conversion.destinationDate.setTime(dateInStartSemester.getTime() + $scope.getDateDifferences());
-            return $scope.conversion.destinationDate;
+            //convert string dates info to JS Date:
 
-        }
+            var dFromSemesterStartDate = new Date(fromSemesterStartDate);
+            var dToSemesterStartDate = new Date(toSemesterStartDate);
+            //dateInStartSemester is already a Date
 
-        $scope.getStructuredDestinationInfo = function (dateInStartSemester) {
-            $scope.conversion.destinationDate = $scope.getDateInDestinationSemester(dateInStartSemester);
 
-            // Weeks X (+1 because the first set of calculation will be 0 index'ed)
-            var weekX = Math.floor(($scope.conversion.destinationDate.dayOfYear() - $scope.conversion.dDestinationSemesterFirstDay.dayOfYear()) / 7) + 1;
+            $scope.destinationDate.setTime(dateInStartSemester.getTime() + (dToSemesterStartDate.getTime() - dFromSemesterStartDate.getTime()));
 
-            return $scope.conversion.outcomeDate = {
+
+            var weekX = Math.floor(($scope.destinationDate.dayOfYear() - dToSemesterStartDate.dayOfYear()) / 7) + 1;
+
+            return $scope.presentedData = {
                 week: weekX,
-                date: $scope.conversion.destinationDate.toUTCString()
+                date: $scope.destinationDate.toUTCString()
             };
         }
 
 
-        $scope.getDateDifferences = function () {
-            return $scope.conversion.dNumberOfDaysDifference = $scope.conversion.dDestinationSemesterFirstDay.getTime() - $scope.conversion.dStartSemesterFirstDay.getTime();
-        }
-
-
-    });
+    }]);
 
 
 
