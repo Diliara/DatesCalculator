ca94c14493f7992532d4dd37950edd75e0b5a2ec
Diliara Nasirova <contact@diliaranasirova.com>
2015-08-28 17:05:58 -0700

    setup json

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -5,48 +5,41 @@ Date.prototype.dayOfYear = function () {
 }
 
 angular.module('datesCalcApp')
-    .controller('datesCalcController', ['$scope', function ($scope) {
+    .controller('datesCalcController', ['$scope', '$http', function ($scope, $http) {
 
-
-        $scope.conversions = [
-            {
-                name: "2015 Spring to 2015 Summer",
-                fromSemester: "2015 Spring - Jan 06",
-                fromSemesterStartDate: "2015-01-06",
-                toSemester: "2015 Summer - May 04",
-                toSemesterStartDate: "2015-05-04"
-            },
-            {
-                name: "2015 Summer to 2015 Fall",
-                fromSemester: "2015 Summer - May 04",
-                fromSemesterStartDate: "2015-05-04",
-                toSemester: "2015 Summer - Sept 08",
-                toSemesterStartDate: "2015-09-08"
-            }
-
-
-        ]
+        $http.get('json/conversions.json').success(function (listOfConversions) {
+            $scope.conversions = listOfConversions;
+        });
 
 
         $scope.getDateInDestinationSemester = function (fromSemesterStartDate, toSemesterStartDate, dateInStartSemester) {
-            $scope.destinationDate = new Date();
-
-            //convert string dates info to JS Date:
-
-            var dFromSemesterStartDate = new Date(fromSemesterStartDate);
-            var dToSemesterStartDate = new Date(toSemesterStartDate);
-            //dateInStartSemester is already a Date
 
 
-            $scope.destinationDate.setTime(dateInStartSemester.getTime() + (dToSemesterStartDate.getTime() - dFromSemesterStartDate.getTime()));
+            if (typeof dateInStartSemester !== "undefined") {
+
+                $scope.destinationDate = new Date();
+
+                //convert string dates info to JS Date:
+
+                var dFromSemesterStartDate = new Date(fromSemesterStartDate);
+                var dToSemesterStartDate = new Date(toSemesterStartDate);
+                //dateInStartSemester is already a Date
 
 
-            var weekX = Math.floor(($scope.destinationDate.dayOfYear() - dToSemesterStartDate.dayOfYear()) / 7) + 1;
+                $scope.destinationDate.setTime(dateInStartSemester.getTime() + (dToSemesterStartDate.getTime() - dFromSemesterStartDate.getTime()));
 
-            return $scope.presentedData = {
-                week: weekX,
-                date: $scope.destinationDate.toUTCString()
-            };
+
+                var weekX = Math.floor(($scope.destinationDate.dayOfYear() - dToSemesterStartDate.dayOfYear()) / 7) + 1;
+
+                return $scope.presentedData = {
+                    week: weekX,
+                    date: $scope.destinationDate.toUTCString()
+                };
+            }
+
+            else {
+                alert("Please, select a date");
+            }
         }
 
 
